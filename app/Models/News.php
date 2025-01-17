6123e54faffd4d14b54a0738712aaae9a12068bc
russell <russell@vpn202.dcs.gla.ac.uk>
2025-01-17 15:03:05 +0000

    news update

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'url',
+        'source_name',
+        'published_at',
+    ];
 }
